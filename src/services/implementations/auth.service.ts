c724162dd31f419e91022c50b9e17bb955ddcfa6
amalm2002 <amalm2495@gmail.com>
2025-09-13 20:12:13 +0530

    refactor : update the data on auth service implementation side

--- src/services/implementations/auth.service.ts
+++ src/services/implementations/auth.service.ts
@@ -5,7 +5,7 @@ import 'dotenv/config';
 import { IAuthService } from '../interfaces/auth.service.interface';
 
 export class AuthService implements IAuthService {
-  private readonly _jwtSecretKey: string;
+  private  _jwtSecretKey: string;
 
   constructor() {
     this._jwtSecretKey = process.env.USER_SECRET_KEY || 'Amal@1';
