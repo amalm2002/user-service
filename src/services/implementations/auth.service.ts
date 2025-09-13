a23e529ea3a492edf5fc58b0f42d2a3503714d15
amalm2002 <amalm2495@gmail.com>
2025-09-13 19:59:50 +0530

    refactor : update the USER_SECRET-KEY data on auth service implementation side

--- src/services/implementations/auth.service.ts
+++ src/services/implementations/auth.service.ts
@@ -8,7 +8,7 @@ export class AuthService implements IAuthService {
   private readonly _jwtSecretKey: string;
 
   constructor() {
-    this._jwtSecretKey = process.env.USER_SECRET_KEY || 'Amal';
+    this._jwtSecretKey = process.env.USER_SECRET_KEY || 'Amal@1';
     if (!process.env.USER_SECRET_KEY) {
       throw new Error('USER_SECRET_KEY is not defined in the ENV file');
     }
