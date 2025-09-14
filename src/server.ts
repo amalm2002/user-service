80dc56a0297d6cc355744de0eb28a2ebfa0414f4
amalm2002 <amalm2495@gmail.com>
2025-09-14 11:26:52 +0530

    refactor : add the console on server page

--- src/server.ts
+++ src/server.ts
@@ -89,6 +89,7 @@ server.addService(userProto.UserService.service, {
 const grpcServer = () => {
   const port = process.env.PORT || '3003';
   const Domain = process.env.NODE_ENV === 'dev' ? process.env.DEV_DOMAIN : process.env.PRO_DOMAIN_USER;
+  console.log('domain :',Domain)
   server.bindAsync(`${Domain}:${port}`, grpc.ServerCredentials.createInsecure(), (err, bindPort) => {
     if (err) {
       console.error("Error starting gRPC server:", err);
